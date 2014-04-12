2815be6f09134df367c22d9915324cd29d00eb00
Hamcha <zikyky@gmail.com>
2014-04-12 20:52:38 +0000

    Get last link

--- mods/flotrshi/url.go
+++ mods/flotrshi/url.go
@@ -11,6 +11,9 @@ import (
 
 var re  *regexp.Regexp
 var re2 *regexp.Regexp
+var lastLink string
+var lastAuthor string
+var lastLinkName string
 func initurl() {
 	fmt.Println("Url Scraper READY!")
 	re  = regexp.MustCompile("http\\S*|www.\\S*")
@@ -19,7 +22,24 @@ func initurl() {
 
 func url(msg Message) {
 	if msg.Command == MESSAGE {
+		if msg.Text == "!last" {
+			send(Message{
+				Command: MESSAGE,
+				Target: msg.Target,
+				Text: "Ultimo link (di "+lastAuthor+"): "+lastLink,
+			})
+			if lastLinkName != "" {
+				send(Message{
+					Command: MESSAGE,
+					Target: msg.Target,
+					Text: "Nome pagina: "+lastLinkName,
+				})
+			}
+		}
 		if url := re.FindString(msg.Text); url != "" {
+			lastLink = url
+			lastAuthor = msg.Source.Nickname
+			lastLinkName = ""
 			res, err := http.Get(url)
 			if err != nil {	return }
 			if head,ok := res.Header["Content-Type"]; ok {
@@ -33,6 +53,7 @@ func url(msg Message) {
 					str = str[7:]
 					str = strings.Replace(str,"</title>","",-1)
 					str = html.UnescapeString(str)
+					lastLinkName = str
 					send(Message{
 						Command: MESSAGE,
 						Target:  msg.Target,
