569f0e15019f47aeb5e89207c0c20f97549d98a9
Hamcha <zikyky@gmail.com>
2014-04-20 23:39:59 +0000

    Added global randst

--- mods/flotrshi/randst.go
+++ mods/flotrshi/randst.go
@@ -11,9 +11,12 @@ var randstX map[string][]string
 
 func initrandst() {
 	bytes, _ := ioutil.ReadFile("s.json")
-	json.Unmarshal(bytes,&randstX)
-
-	fmt.Println("Randst loaded!")
+	err := json.Unmarshal(bytes,&randstX)
+	if err != nil {
+		fmt.Println("RANDST ERROR: "+err.Error())
+	} else {
+		fmt.Println("Randst loaded!")
+	}
 }
 
 const PROBOF = 2
@@ -21,16 +24,20 @@ const PROBIN = 10
 
 func randst(msg Message) {
 	if (msg.Command == MESSAGE) {
-		if msgs, ok := randstX[msg.Source.Nickname]; ok {
-			if rand.Intn(PROBIN) == PROBOF {
-				idx := rand.Intn(len(msgs))
-				msx := msgs[idx]
-				send(Message{
-					Command: MESSAGE,
-					Target:  msg.Target,
-					Text:    msx,
-				})
+		if rand.Intn(PROBIN) == PROBOF {
+			var msgs []string
+			if msgx, ok := randstX[msg.Source.Nickname]; ok && rand.Intn(3) < 2 {
+				msgs = msgx
+			} else {
+				msgs = randstX["*"]
 			}
+			idx := rand.Intn(len(msgs))
+			msx := msgs[idx]
+			send(Message{
+				Command: MESSAGE,
+				Target:  msg.Target,
+				Text:    msx,
+			})
 		}
 	}
 }
